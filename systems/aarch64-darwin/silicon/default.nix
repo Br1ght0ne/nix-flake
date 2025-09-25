a70e76b66156b0c906e14939100925d67ceff44d
Oleksii Filonenko <brightone@proton.me>
2025-09-25 17:31:53 +0100

    Split silicon.system into system.nix

--- systems/aarch64-darwin/silicon/default.nix
+++ systems/aarch64-darwin/silicon/default.nix
@@ -1,23 +1,5 @@
 {...}: {
-  imports = [./aerospace.nix ./homebrew.nix];
+  imports = [./aerospace.nix ./homebrew.nix ./system.nix];
   # Don't manage Determinate Nix
   nix.enable = false;
-  system.stateVersion = 6;
-
-  system.primaryUser = "brightone";
-
-  system.defaults = {
-    menuExtraClock = {
-      Show24Hour = true;
-      ShowDate = 2; # never
-      ShowDayOfWeek = false;
-      ShowDayOfMonth = false;
-    };
-  };
-
-  system.keyboard = {
-    enableKeyMapping = true;
-    nonUS.remapTilde = true;
-    nonUS.remapComparisonSigns = true;
-  };
 }
---
+++ systems/aarch64-darwin/silicon/system.nix
@@ -0,0 +1,17 @@
+{
+  system.stateVersion = 6;
+  system.primaryUser = "brightone";
+  system.defaults = {
+    menuExtraClock = {
+      Show24Hour = true;
+      ShowDate = 2; # never
+      ShowDayOfWeek = false;
+      ShowDayOfMonth = false;
+    };
+  };
+  system.keyboard = {
+    enableKeyMapping = true;
+    nonUS.remapTilde = true;
+    nonUS.remapComparisonSigns = true;
+  };
+}
