a58b0421f4fd1dc400c7e364b5039da76d3ade77
Oleksii Filonenko <brightone@proton.me>
2026-04-29 17:24:13 +0100

    zed settings update

--- homes/aarch64-darwin/brightone/zed.nix
+++ homes/aarch64-darwin/brightone/zed.nix
@@ -11,23 +11,80 @@
     package = inputs.nixpkgs-stable.legacyPackages.${system}.zed-editor;
     extraPackages = with pkgs; [bash-language-server nil nixd];
     userSettings = {
-      edit_predictions = {
-        mode = "eager";
-        copilot = lib.genAttrs ["proxy" "proxy_no_verify" "enterprise_uri"] (_: null);
-        enabled_in_text_threads = false;
+      cli_default_open_behavior = "new_window";
+      project_panel.dock = "right";
+      outline_panel.dock = "right";
+      collaboration_panel.dock = "right";
+      git_panel.dock = "right";
+      diff_view_style = "unified";
+      focus_follows_mouse.enabled = false;
+      agent_servers = {
+        "pi-acp".type = "registry";
+        "claude-acp".type = "registry";
       };
-      ssh_connections = [
-        {
-          host = "bazzite";
-          username = "brightone";
-          projects = [
-            {
-              paths = ["/home/brightone/code/syndicate-appchains"];
-            }
-          ];
-        }
-      ];
-      # helix_mode = true;
+      context_servers = {
+        "mcp-server-github" = {
+          enabled = false;
+          settings = {};
+        };
+        kagimcp = {
+          enabled = false;
+          settings = {
+            kagi_api_key = "qa-JoxtgPScDSQMMkWos9cZpXY9KY29ttjS7fdrvq_o.p0IJtrdoOmzf-MBGMoyZxNbSQwCcDDbzpImakJDy5-c";
+            kagi_summarizer_engine = "cecil";
+            kagi_fastgpt_cache = true;
+            kagi_fastgpt_web_search = true;
+          };
+        };
+        "mcp-server-context7" = {
+          enabled = true;
+          settings.context7_api_key = "ctx7sk-3c15f0e7-b151-49a2-b88a-d459f4ede12b";
+        };
+      };
+      colorize_brackets = true;
+      agent = {
+        dock = "left";
+        tool_permissions.default = "allow";
+        inline_assistant_model = {
+          provider = "copilot_chat";
+          model = "claude-sonnet-4.6";
+        };
+        favorite_models = [
+          {
+            provider = "copilot_chat";
+            model = "claude-opus-4.5";
+            enable_thinking = false;
+          }
+          {
+            provider = "copilot_chat";
+            model = "claude-opus-4.6";
+            enable_thinking = false;
+          }
+          {
+            provider = "copilot_chat";
+            model = "claude-sonnet-4.6";
+            enable_thinking = true;
+            effort = "high";
+          }
+          {
+            provider = "copilot_chat";
+            model = "claude-sonnet-4.5";
+            enable_thinking = false;
+          }
+          {
+            provider = "copilot_chat";
+            model = "gpt-5.3-codex";
+            enable_thinking = false;
+          }
+        ];
+        default_model = {
+          effort = "high";
+          enable_thinking = true;
+          model = "claude-sonnet-4.6";
+          provider = "copilot_chat";
+        };
+        default_profile = "write";
+      };
       vim_mode = true;
       ui_font_size = 14;
       buffer_font_size = 13;
@@ -41,25 +98,45 @@
         env.EDITOR = "zed --wait";
         shell.program = "nu";
       };
-      agent = {
-        default_profile = "ask";
-        default_model = {
-          provider = "copilot_chat";
-          model = "claude-3.7-sonnet";
-        };
+      edit_predictions = {
+        provider = "copilot";
+        mode = "eager";
+        copilot = lib.genAttrs ["proxy" "proxy_no_verify" "enterprise_uri"] (_: null);
+        enabled_in_text_threads = false;
       };
-      features.edit_prediction_provider = "zed";
       use_smartcase_search = true;
       inlay_hints.enabled = true;
-      languages.Nix.language_servers = ["nixd" "nil"];
-      lsp.typos.initialization_options = {
-        diagnosticSeverity = "Warning";
-        logLevel = "info";
+      languages = {
+        Nix.language_servers = ["nixd" "nil"];
+        Solidity.tab_size = 2;
       };
-      lsp.rust-analyzer.initialization_options.check.command = "clippy";
-      lsp.nil.settings.formatting.command = ["nix" "fmt"];
-      lsp.nixd.settings.formatting.command = ["nix" "fmt"];
       diagnostics.inline.enabled = true;
+      lsp = {
+        typos.initialization_options = {
+          diagnosticSeverity = "Warning";
+          logLevel = "info";
+        };
+        "rust-analyzer" = {
+          binary = {
+            path = "/Users/brightone/.local/share/cargo/bin/rust-analyzer";
+            arguments = [];
+          };
+          initialization_options.check.command = "clippy";
+        };
+        nil.settings.formatting.command = ["nix" "fmt"];
+        nixd.settings.formatting.command = ["nix" "fmt"];
+      };
+      ssh_connections = [
+        {
+          host = "bazzite";
+          username = "brightone";
+          projects = [
+            {
+              paths = ["/home/brightone/code/syndicate-appchains"];
+            }
+          ];
+        }
+      ];
     };
   };
 }
